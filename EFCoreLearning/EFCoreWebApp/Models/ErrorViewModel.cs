389a9b90d4e0428b50d3833cec60c492552afbf3
Arslan Ahmad <arslan.ahmad@imanami.com>
2021-06-26 17:06:07 +0500

    Added the solution and the driver project

---
+++ EFCoreLearning/EFCoreWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EFCoreWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
